10d0006aab77178287c88485321b423559d56bd8
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-07-10 16:45:55 +0700

    Add selectable date in AddTransactionForm
    * Add DatePicker component
    * Integrate DatePicker in AddTransactionForm

--- src/actions/transaction.actions.ts
+++ src/actions/transaction.actions.ts
@@ -8,13 +8,14 @@ export async function createTransaction(formData: FormData) {
   const type = formData.get("type") as "income" | "expense";
   const category = (formData.get("category") as string) || "Misc";
   const note = formData.get("note") as string;
+  const date = formData.get("date") as string;
 
   if (!amount || !type) {
     throw new Error("Amount dan type harus diisi");
   }
 
   await prisma.transaction.create({
-    data: { amount, type, category, note },
+    data: { amount, type, category, note, date: new Date(date) },
   });
 
   revalidatePath("/transactions");
--- src/components/transaction/AddTransactionForm.tsx
+++ src/components/transaction/AddTransactionForm.tsx
@@ -3,8 +3,10 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { createTransaction } from "@/actions/transaction.actions";
+import { DatePicker } from "../DatePicker";
 
 export function AddTransactionForm() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
   const [amount, setAmount] = useState("");
   const [type, setType] = useState<"income" | "expense">("expense");
 
@@ -18,6 +20,7 @@ export function AddTransactionForm() {
         onChange={(e) => setAmount(e.target.value)}
         name="amount"
       />
+      <DatePicker value={date} onChange={setDate} />
       <div className="flex gap-2 my-2">
         <Button
           variant={type === "expense" ? "default" : "outline"}
@@ -34,9 +37,11 @@ export function AddTransactionForm() {
       </div>
       <Button
         onClick={async () => {
+          console.log({ date });
           const formData = new FormData();
           formData.append("amount", amount.toString());
           formData.append("type", type);
+          formData.append("date", date?.toISOString() || "");
           await createTransaction(formData);
         }}
       >
