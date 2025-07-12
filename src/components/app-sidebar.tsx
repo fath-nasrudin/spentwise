62f8cd3ecd52d1b7f7cf93a71a4a23fd565f9e6d
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-07-13 06:14:18 +0700

    Add sidebar

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,85 @@
+import {
+  ArrowLeftRightIcon,
+  Calendar,
+  Inbox,
+  Search,
+  Settings,
+  TagsIcon,
+  WalletIcon,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Categories",
+    url: "/categories",
+    icon: TagsIcon,
+  },
+  {
+    title: "Transactions",
+    url: "/transactions",
+    icon: ArrowLeftRightIcon,
+  },
+  {
+    title: "Wallets",
+    url: "/wallets",
+    icon: WalletIcon,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <span className="text-base font-semibold">Spentwise</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
