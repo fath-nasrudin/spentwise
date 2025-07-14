957c5b7cf90f75127b53d892bc0fe870cfdcf13e
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-07-14 17:29:40 +0700

    Update the UI
    * Add the header
    * Make the add transaction as dialog instead of directly showing the form

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,9 +1,10 @@
 import BalanceSummary from "@/components/BalanceSummary";
 import { getUserTransactions } from "./transaction.actions";
-import { AddTransactionForm } from "@/app/transactions/AddTransactionForm";
 import { TransactionList } from "@/app/transactions/TransactionList";
 import { getUserCategories } from "../categories/category.db";
 import { getUserWallets } from "../wallets/wallet.actions";
+
+import { AddTransactionFormDialogButton } from "./add-transaction-form-dialog";
 
 export default async function TransactionsPage() {
   const { data } = await getUserTransactions();
@@ -11,13 +12,33 @@ export default async function TransactionsPage() {
   let { data: walelts } = await getUserWallets();
 
   if (!data) {
-    return <div className="p-4">Tidak ada transaksi yang ditemukan.</div>;
+    return <div className="p-4">No transaction found.</div>;
   }
   return (
-    <div className="p-4 max-w-7xl mx-auto">
-      <AddTransactionForm categories={categories} wallets={walelts} />
-      <BalanceSummary />
-      <TransactionList data={data} />
+    <div className="container w-full max-w-7xl mx-auto p-6 space-y-6">
+      {/* Header */}
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="text-3xl font-bold">Transactions</h1>
+          <p className="text-muted-foreground">
+            See your whole transactions here
+          </p>
+        </div>
+        <AddTransactionFormDialogButton
+          categories={categories}
+          wallets={walelts}
+        />
+      </div>
+
+      {/*  */}
+      <div>
+        <BalanceSummary />
+      </div>
+
+      {/*  */}
+      <div>
+        <TransactionList data={data} />
+      </div>
     </div>
   );
 }
