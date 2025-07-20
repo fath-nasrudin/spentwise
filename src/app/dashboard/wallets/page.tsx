53be0d4083f93ca0cf986cd77615b70e3118bee3
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-07-20 16:52:39 +0700

    Add metadata for pages

--- src/app/dashboard/wallets/page.tsx
+++ src/app/dashboard/wallets/page.tsx
@@ -1,5 +1,10 @@
 import { getUserWalletsWithBalance } from "./wallet.actions";
 import { WalletsPageClient } from "./page.client";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Wallets",
+};
 
 export default async function WalletsPage() {
   const { data: wallets } = await getUserWalletsWithBalance();
