6083b6101ff49b5a9b36aac86c8d84f908437eec
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-07-29 16:21:39 +0700

    Add React Query Devtools

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider } from "@/lib/query/client";
 
 export function Providers({
@@ -7,5 +7,9 @@ export function Providers({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <QueryClientProvider>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider>
+      {children} <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
